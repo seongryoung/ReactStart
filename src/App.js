043368337169e2e6f4aa0e99e55f8a4e3dcf11ef
seongryoung <sholyspirit2@naver.com>
2019-05-04 15:52:55 +0900

    5/4일 공부내용

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class Subject extends Component{
   render(){
     return(
-      <div>
-        <h1>{this.props.title}</h1>
+      <div className='subject'>
+        <h1><a href="/" onClick={function(ev){
+          ev.preventDefault();
+          this.props.onClick();
+        }.bind(this)}>{this.props.title}</a></h1>
         {this.props.sub}
       </div>
     );
@@ -54,6 +57,7 @@ class Contents extends Component{
 
 class App extends Component {
   state = {
+    mode : 'welcome',
     selected_content_id : 1,
     contents : [
       {id:1, title:'HTML', desc: "html is for information"},
@@ -69,14 +73,31 @@ class App extends Component {
       }
     }
   }
+
+  getConetentComponent(){
+    if(this.state === 'read'){
+      return <Contents data={this.getSelectedContent()}></Contents>
+    }else if(this.state === 'welcome'){
+      return <Contents data={{
+        title : 'welcome',
+        mode : 'read'
+      }}></Contents>
+    }
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title='WEB' sub='World wide web'></Subject>
+        <Subject onClick={function(){
+          this.setState({mode : 'Weclome'})
+        }.bind(this)} title='WEB' sub='World wide web'></Subject>
+
+        
 
         <TOC onSelect={function(id){
           console.log('App', id);
-          this.setState({
+          this.setState({//이렇게 해줘야 리액트가 일을한다.
             selected_content_id : id
           })
         }.bind(this)} data={this.state.contents}></TOC>
@@ -89,3 +110,6 @@ class App extends Component {
 }
 
 export default App;
+// Sever Start : npm start; 테스트 할때
+// npm run build : 배포시 이걸로 빌드해서 배포해야함 배포해야함
+//실행법 : npx serve -s build
